a727535b26f4f4a32f10729192fac54ffb54f9bf
MertOzerdem <32201738+MertOzerdem@users.noreply.github.com>
2024-05-29 13:14:33 +0300

    chore: PONG returns for PING

--- app/main.js
+++ app/main.js
@@ -3,9 +3,23 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  // Handle connection
+    console.log('Client connected');
+
+    connection.on('data', (data) => {
+        console.log("data", data.toString().trim())
+        const command = data.toString().trim();
+        if (command.includes('PING')) {
+            connection.write('+PONG\r\n');
+            console.log('PONG sent to client');
+        }
+    });
+
+    connection.on('end', () => {
+        console.log('Client disconnected');
+    });
 });
 
 server.listen(6379, "127.0.0.1");
+
+
