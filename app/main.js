57aaad8cc379a4955888af12339f107ae27724e2
MertOzerdem <32201738+MertOzerdem@users.noreply.github.com>
2024-05-29 16:48:32 +0300

    chore: Uncomment server creation code in main.js

--- app/main.js
+++ app/main.js
@@ -3,9 +3,6 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// $<length>\r\n<data>\r\n
-// $3\r\nhey\r\n
-
 let store = {};
 
 const server = net.createServer((connection) => {
